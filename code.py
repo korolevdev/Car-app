cdb59d9691f0687c747e6115bc478b85346f13ed
korolevdev <korolevdev@korolevdev@gmail.com>
2016-11-30 17:19:06 +0000

    change decode func

--- code.py
+++ code.py
@@ -15,72 +15,65 @@ GPIO.setup(rr2, GPIO.OUT, initial=0)
 GPIO.setup(lr1, GPIO.OUT, initial=0)
 GPIO.setup(lr2, GPIO.OUT, initial=0)
 
-left = GPIO.PWM(lb, 50)
-right = GPIO.PWM(rb, 50)
+leftb = GPIO.PWM(lb, 50)
+rightb = GPIO.PWM(rb, 50)
 
-left.start(0)
-right.start(0)
-
-left.ChangeDutyCycle(100)
-right.ChangeDutyCycle(100)
+leftb.start(0)
+rightb.start(0)
 
 def decode(packet):
     dest = packet >> 24
     speed = (packet >> 16) & 0xff
     return dest, speed
 
-def forward():
-	GPIO.output(rr1, 1)
-	GPIO.output(rr2, 0)
-	GPIO.output(lr1, 0)
-	GPIO.output(lr2, 1)
+def set_speed(speed):
+	leftb.ChangeDutyCycle(speed)
+	rightb.ChangeDutyCycle(speed)
 
-def backward():
-	GPIO.output(rr1, 1)
-	GPIO.output(rr2, 0)
-	GPIO.output(lr1, 0)
-	GPIO.output(lr2, 1)	
+def parse_command(com):
+	if com == 1:
+		forward()
+	elif com == 2:
+		backward()
+	elif com == 3:
+		left()
+	elif com == 4:
+		right()
 
 def left():
 	GPIO.output(rr1, 1)
 	GPIO.output(rr2, 0)
-	GPIO.output(lr1, 1)	
-	GPIO.output(lr2, 0)
+	GPIO.output(lr1, 0)
+	GPIO.output(lr2, 1)
 
 def right():
 	GPIO.output(rr1, 0)
 	GPIO.output(rr2, 1)
-	GPIO.output(lr1, 0)
-	GPIO.output(lr2, 1)
+	GPIO.output(lr1, 1)
+	GPIO.output(lr2, 0)
 
-def parse_command(com):
-    return {
-		com == 1:
-			forward(),
-		com == 2:
-			backward(),
-		com == 3:
-			left(),
-		com == 4:
-			right(),
-    }[True]
-
-#sock = socket.socket()
-#sock.connect(('localhost', 9093))
-
-while 1:
-	#parse_command(1)
-	#forward()
-	left.ChangeDutyCycle(100)
-	right.ChangeDutyCycle(100)
-	print '1'
+def forward():
 	GPIO.output(rr1, 0)
 	GPIO.output(rr2, 1)
 	GPIO.output(lr1, 0)
-	GPIO.output(lr2, 1)
+	GPIO.output(lr2, 1)	
+
+def backward():
+	GPIO.output(rr1, 1)
+	GPIO.output(rr2, 0)
+	GPIO.output(lr1, 1)
+	GPIO.output(lr2, 0)
+
+sock = socket.socket()
+sock.connect(('localhost', 9093))
+
+while 1:
+	data = sock.recv(1024)
+	dest, speed = decode(int(data))
+	parse_command(dest)
+	set_speed(speed)
 	time.sleep(0.1)
 
-sock.close()
 left.stop()
 right.stop()
 GPIO.cleanup()
