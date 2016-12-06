d8478e2a7e84292500d891abe6bc1a22b1823bee
Владимир Бибин <devbibinva@gmail.com>
2016-12-07 01:58:49 +0300

    add myo_control.py tests

--- tests/check_command.py
+++ tests/check_command.py
@@ -1,1 +1,66 @@
-from ../myo_control import *
+# some_file.py
+import sys
+sys.path.insert(0, '../')
+from myo_control import *
+
+#TODO borders
+
+def test1_get_command_dest():
+	tmp = -1.2
+	comm = get_command_dest(tmp)
+	if (comm == 2):
+		print("Backward|OK")
+	else:
+		print("Backward|FAIL")
+
+
+def test2_get_command_dest():
+	tmp = 0
+	comm = get_command_dest(tmp)
+	if (comm == 0):
+		print("Stop|OK")
+	else:
+		print("Stop|FAIL")
+
+def test3_get_command_dest():
+	tmp = 1.2
+	comm = get_command_dest(tmp)
+	if (comm == 1):
+		print("Forward|OK")
+	else:
+		print("Forward|FAIL")
+
+def test1_get_command_turn():
+	tmp = -0.5
+	comm = get_command_dest(tmp)
+	if (comm == 3):
+		print("Left|OK")
+	else:
+		print("Left|FAIL")
+
+def test2_get_command_turn():
+	tmp = 0
+	comm = get_command_dest(tmp)
+	if (comm == 5):
+		print("Toward|OK")
+	else:
+		print("Toward|FAIL")
+
+def test3_get_command_turn():
+	tmp = 0.5
+	comm = get_command_dest(tmp)
+	if (comm == 4):
+		print("Right|OK")
+	else:
+		print("Right|FAIL")
+
+def test_myo_control():
+	print ("Testing get_command_dest:")
+	test1_get_command_dest()
+	test2_get_command_dest()
+	test3_get_command_dest()
+	print ("Testing get_command_turn:")
+	test1_get_command_turn()
+	test2_get_command_turn()
+	test3_get_command_turn()
+	
