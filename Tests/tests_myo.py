c0f3f4aff9fe0808c43f0108798bf08f7573fc32
Владимир Бибин <devbibinva@gmail.com>
2016-12-22 17:03:11 +0300

    Tests for myo

--- Tests/tests_myo.py
+++ Tests/tests_myo.py
@@ -1,78 +1,79 @@
-# some_file.py
 import sys
 sys.path.insert(0, '../')
+from myo_raw import *
 from myo_control import *
-from manager import myo_connect
 
-#TODO borders
+flags = [1, 0, 0, 0, 0, 0]
+
+def proc_imu(quat, acc, gyro, times=[]):
+	roll = get_roll(quat)
+	pitch = get_pitch(quat)
+	if flags[0] == 1:
+		test2_myo(roll,pitch)
+	if flags[1] == 1:
+		test3_myo(roll,pitch)
+	if flags[2] == 1:
+		test4_myo(roll,pitch)
+	if flags[3] == 1:
+		test5_myo(roll,pitch)
+	if flags[4] == 1:
+		test6_myo(roll,pitch)
+	if flags[5] == 1:
+		test7_myo(roll,pitch)
+	if flags == 0:
+		print("TESTS OK")
+
 
 def test1_myo():
-	myo = myo_connect()
+	myo = MyoRaw(sys.argv[1] if len(sys.argv) >= 2 else None)
+	myo.connect()
+	myo.add_imu_handler(proc_imu)
 	print("Connection|OK")
-	myo.disconnect()
+	return myo
 
-myo = myo_connect()
+def test2_myo(roll,pitch):	
+	if (get_myo_turn(roll) == 3):
+		print("Turn left|OK")
+		flags[0] = 0
+		flags[1] = 1
+		
 
-def test2_myo():
-	while True:	
-		myo.run()
-		turn = get_roll()
-		if (get_myo_turn(turn) == 3):
-			print("Turned left|OK")
-			break
-def test3_myo():
-	while True:	
-		myo.run()
-		turn = get_roll()
-		if (get_myo_turn(turn) == 5):
-			print("Toward|OK")
-			break
-def test4_myo():
-	while True:	
-		myo.run()
-		turn = get_roll()
-		if (get_myo_turn(turn) == 4):
-			print("Turned right|OK")
-			break
+def test3_myo(roll,pitch):
+	if (get_myo_turn(roll) == 5):
+		print("Toward|OK")
+		flags[1] = 0
+		flags[2] = 1
+		
+def test4_myo(roll,pitch):
+	if (get_myo_turn(roll) == 4):
+		print("Turn right|OK")
+		flags[2] = 0
+		flags[3] = 1
 
-def test5_myo():
-	while True:	
-		myo.run()
-		dest = get_pitch()
-		if (get_myo_dest(dest) == 1):
-			print("Forward|OK")
-			break
-def test6_myo():
-	while True:	
-		myo.run()
-		dest = get_pitch()
-		if (get_myo_dest(dest) == 0):
-			print("Stop|OK")
-			break
+def test5_myo(roll,pitch):
+	if (get_myo_dest(pitch) == 1):
+		print("Forward|OK")
+		flags[3] = 0
+		flags[4] = 1
 
-def test7_myo():
-	while True:	
-		myo.run()
-		dest = get_pitch()
-		if (get_myo_dest(dest) == 2):
-			print("Backward|OK")
-			break
+def test6_myo(roll,pitch):
+	if (get_myo_dest(pitch) == 0):
+		print("Stop|OK")
+		flags[4] = 0
+		flags[5] = 1
+		
 
-def test_myo():
-	print("Testing myo:")
-	print("Waiting connection:")	
-	test1_myo()
-	print ("Waiting left:")
-	test2_myo()
-	print ("Waiting toward:")
-	test3_myo()
-	print ("Waiting right:")
-	test4_myo()
-	print ("Waiting forward:")
-	test5_myo()
-	print ("Waiting stop:")
-	test6_myo()
-	print ("Waiting backward:")
-	test7_myo()
+def test7_myo(roll,pitch):
+	if (get_myo_dest(pitch) == 2):
+		print("Backward|OK")
+		flags[5] = 0
 
-test_myo()
+try:
+	print("Waiting connection")
+	myo = test1_myo()
+	while True:
+		myo.run(1) 
+except KeyboardInterrupt:
+	pass
+		
+
