7987d95935e5df472d79caae1d87e2ed583e7b1e
korolevdev <korolevdev@gmail.com>
2016-12-28 19:53:53 +0300

    utils.py refact

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ def encode(a, b, c):
 		return int(a) << 24 | int(b) << 16 | int(c) & 0xffff;
 	except ValueError:
 		print "Encode only int"
+
 def decode(packet):
 	a = packet >> 24
 	b = (packet >> 16) & 0xff
@@ -18,4 +19,6 @@ def check_int(str):
         return False
 
 def perform_json(real_speed, range, teor_speed, route_car, route_myo):
-	return '{"real_speed":' + str(real_speed) + ',"range":' + str(range) + ',"teor_speed":' + str(teor_speed) + ',"route_car":' + str(route_car) + ',"route_myo":' + str(route_myo) + '}'
+	return '{"real_speed":' + str(real_speed) + ',"range":' + str(range) + 
+			',"teor_speed":' + str(teor_speed) + ',"route_car":' + str(route_car) + 
+			',"route_myo":' + str(route_myo) + '}'
